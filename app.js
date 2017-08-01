0f788a9c95e4bf13be97b99083d72895603cb3b3
gpciancio <gopalciancio@gmail.com>
2017-08-01 13:44:18 -0600

    push to heroku

--- app.js
+++ app.js
@@ -20,21 +20,14 @@ function initMap() {
       });
   }
 
-  // click event listening for a click on the map
   map.addListener("click", function(event) {
-    // set the lat and lon of the click coordinates
     latitude = event.latLng.lat();
     longitude = event.latLng.lng();
 
-    // find the '#photos' element in our html in order to update if photos are loaded
     var photosElement = document.getElementById('photos');
-
-    // set up requestUrl with lat and lon variables and API key variable
     var requestUrl = `https://gp-flickr.herokuapp.com/services/rest/?method=flickr.photos.search&lat=${latitude}&lon=${longitude}&format=json&nojsoncallback=1`;
 
-    // make request to Flicker using requestUrl to get photos by lat and lon
     $.get(requestUrl, function(result) {
-      // TODO -- loop through the result.photos.photo array to get all the photos
       var farmId;
       var serverId;
       var secret;
@@ -42,21 +35,16 @@ function initMap() {
       var photoArray;
       var randomPhoto
 
-      // set variables to be used to build the image url to display the images
       if (result.photos.photo.length > 0) {
         placeMarker(event.latLng);
-
         photoArray = result.photos.photo;
         randomPhoto = photoArray[Math.floor(Math.random() * photoArray.length)]
         farmId = randomPhoto.farm;
         serverId = randomPhoto.server;
         id = randomPhoto.id;
         secret = randomPhoto.secret;
-
-        // imageUrl that will be used to display the image in the html below the map
         imageUrl = `https://farm${farmId}.staticflickr.com/${serverId}/${id}_${secret}.jpg`
 
-        // create the image element so it can be appended to the #below element -- 'elem'
         var img = document.createElement("IMG");
         img.setAttribute('src', imageUrl);
         img.setAttribute('width', '200px');
